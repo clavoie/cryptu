e43f2e02cd08da51cce056324e168cd485158116
Chris LaVoie <clavoie@gmail.com>
2018-08-31 08:54:01 -0400

    All tests pass now

---
+++ global_test.go
@@ -0,0 +1,51 @@
+package cryptu_test
+
+import (
+	"encoding/base64"
+	"strings"
+	"testing"
+
+	"github.com/clavoie/cryptu"
+)
+
+func TestGlobal(t *testing.T) {
+	t.Run("InvalidKey", func(t *testing.T) {
+		if _, err := cryptu.EncryptToBase64("", "hello"); err == nil {
+			t.Fatal("Expecting error")
+		}
+
+		if _, err := cryptu.DecryptFromBase64("invalid key", "hello"); err == nil {
+			t.Fatal("Expecting error")
+		}
+	})
+
+	t.Run("EncryptDecrypt", func(t *testing.T) {
+		key := strings.Repeat("a", 16)
+		secret := "myt secret"
+
+		encryptedValue, err := cryptu.EncryptToBase64(key, secret)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		decodedValue, err := base64.StdEncoding.DecodeString(encryptedValue)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if encryptedValue == secret || string(decodedValue) == secret {
+			t.Fatal("Not encrypted", encryptedValue, secret, string(decodedValue))
+		}
+
+		actualValue, err := cryptu.DecryptFromBase64(key, encryptedValue)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if actualValue != secret {
+			t.Fatal(actualValue, secret)
+		}
+	})
+}
--- key_test.go
+++ key_test.go
@@ -2,78 +2,26 @@ package cryptu_test
 
 import (
 	"bytes"
-	"strings"
 	"testing"
 
 	"github.com/clavoie/cryptu"
 )
 
 func TestKey(t *testing.T) {
-	key16 := strings.Repeat("a", 16)
-	key24 := strings.Repeat("b", 24)
-	key32 := strings.Repeat("c", 32)
+	_, err := cryptu.NewStrKey("")
 
-	t.Run("NewKey16", func(t *testing.T) {
-		key, err := cryptu.NewKey([]byte(key16))
+	if err == nil {
+		t.Fatal("Was expecting err")
+	}
 
-		if err != nil {
-			t.Fatal(err)
-		}
+	strKey := "my key"
+	key, err := cryptu.NewStrKey(strKey)
 
-		if bytes.Equal(key.Bytes(), []byte(key16)) == false {
-			t.Fatal(key.Bytes(), key16)
-		}
-	})
-	t.Run("NewKey24", func(t *testing.T) {
-		key, err := cryptu.NewKey([]byte(key24))
+	if err != nil {
+		t.Fatal(err)
+	}
 
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if bytes.Equal(key.Bytes(), []byte(key24)) == false {
-			t.Fatal(key.Bytes(), key24)
-		}
-	})
-	t.Run("NewKey32", func(t *testing.T) {
-		key, err := cryptu.NewKey([]byte(key32))
-
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if bytes.Equal(key.Bytes(), []byte(key32)) == false {
-			t.Fatal(key.Bytes(), key32)
-		}
-	})
-	t.Run("NewKeyInvalid", func(t *testing.T) {
-		_, err := cryptu.NewKey([]byte(key16 + "A"))
-
-		if err == nil {
-			t.Fatal("Was expecting err")
-		}
-
-		_, err = cryptu.NewKey(nil)
-
-		if err == nil {
-			t.Fatal("Was expecting err")
-		}
-	})
-	t.Run("NewStrKey", func(t *testing.T) {
-		_, err := cryptu.NewStrKey("")
-
-		if err == nil {
-			t.Fatal("Was expecting err")
-		}
-
-		key, err := cryptu.NewStrKey(key16)
-
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if string(key.Bytes()) != key16 {
-			t.Fatal(string(key.Bytes()), key16)
-		}
-	})
+	if bytes.Equal([]byte(strKey), key.Bytes()) == false {
+		t.Fatal("Keys do not match", strKey, string(key.Bytes()))
+	}
 }
